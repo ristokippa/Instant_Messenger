e09185931921b1fe9ae3e5b026039241a2aa9d9f
DavidRandoja <david.randoja@gmail.com>
2015-09-30 21:34:51 +0300

    Initial sync
    Added automatic ip_request and datetime.now.

--- ServerSide_Development/ServerSide/ServerSide/Classes/Server.cs
+++ ServerSide_Development/ServerSide/ServerSide/Classes/Server.cs
@@ -21,19 +21,40 @@ namespace ServerSide.Classes
             y = l;
         }
 
-        public void PrintStuff(string text) 
+        public void PrintStuff(string text)
         {
             Console.WriteLine(text);
+
         }
 
-        public bool IsConnected(string client_ip, int client_socket)
+        public string IpRequest()
+        {
+            // Getting IP address from local PC
+            String strHostName = string.Empty;
+            // First get host name from local machine
+            strHostName = Dns.GetHostName();
+            // Then using host name, get IP address from a list
+            Console.WriteLine("[{0}] Local PC's host name: " + strHostName, DateTime.Now);
+
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            // Put together the desired IP address
+            //for (int i = 0; i < addr.Length; i++)
+            //{
+            //    //Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            //    //Console.WriteLine("====>" + addr[1]);
+            //}
+            return addr[1].ToString();
+        }
+
+        public bool IsConnected(string server_ip, int client_socket)
         {
             try
             {
-                Console.WriteLine("Waiting for connection with device (IP: " + client_ip + ", Port: " + client_socket + ")");
+                Console.WriteLine("Waiting for connection with device (IP: " + server_ip + ", Port: " + client_socket + ")");
 
                 // Uses the GetStream public method to return the NetworkStream.
-                System.Net.IPAddress ipaddress = System.Net.IPAddress.Parse(client_ip);
+                System.Net.IPAddress ipaddress = System.Net.IPAddress.Parse(server_ip);
                 TcpListener serverSocket = new TcpListener(ipaddress, client_socket);
                 serverSocket.Start(); //Initializes TcpListner
 
@@ -47,16 +68,24 @@ namespace ServerSide.Classes
 
             catch (Exception ex)
             {
-                Console.WriteLine("WOOT JUST HAPPND?!");
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine("///////////////////////////////////////////////////////////////////////////////\n");
+                Console.WriteLine("EXCEPTION: ");
+                Console.WriteLine(ex.ToString() + "\n");
+                Console.WriteLine("///////////////////////////////////////////////////////////////////////////////");
                 return false;
             }
 
         }
+
         public void PortListener()
         {
-            
+            //Write code for incoming messages
+        }
+
+
+        public void GlobalVariables()
+        {
+            string IPAddr = "IpReq result";
         }
     }
 }
-
--- ServerSide_Development/ServerSide/ServerSide/Program.cs
+++ ServerSide_Development/ServerSide/ServerSide/Program.cs
@@ -11,19 +11,17 @@ namespace ServerSide // miks on vaja classes folderit_?
     {
         static void Main(string[] args)
         {
-            Classes.Server uus = new Classes.Server(5, 4);
-            uus.PrintStuff("Server started.");
-            //string ip = "127.0.0.1";
-            //string ip = "192.168.43.183"; //dell
-            string ip = "192.168.43.254";   //msi
-            Console.WriteLine("Current server IP = " + ip);
-            string port = "10000";
-            Console.WriteLine("Current target port = " + port);
 
-            uus.IsConnected(ip, Convert.ToInt32(port));
+            Classes.Server ServerSide = new Classes.Server(5, 4);
+            // Just for practise
+
+            ServerSide.PrintStuff("Server started.");
+            // IpRequest returns local PC-s IPaddress
+            string ip = ServerSide.IpRequest();
+            string port = "10000";
+            ServerSide.IsConnected(ip, Convert.ToInt32(port));
             //Console.WriteLine(Marshal.SizeOf(uus));   //Cannot use it. Solution: http://stackoverflow.com/questions/19408424/why-cant-i-do-marshal-sizeof-for-this-c-sharp-struct
-            
-            Console.WriteLine("The end");
+            Console.WriteLine("[{0}] Main program finished.", DateTime.Now);
             Console.Read();
 
         }
